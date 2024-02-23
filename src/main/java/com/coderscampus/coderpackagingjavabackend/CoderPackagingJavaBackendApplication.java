db22d493ff882781f4e75dfc7210925c03a8c583
JP-CC-Projects <joeparkcc@gmail.com>
2024-02-23 13:29:59 -0600

    first commit

---
+++ src/main/java/com/coderscampus/coderpackagingjavabackend/CoderPackagingJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.coderpackagingjavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderPackagingJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderPackagingJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/coderpackagingjavabackend/CoderPackagingJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.coderpackagingjavabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderPackagingJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
